b12dc6ffd9c815f6ec74330c8de4a45ae1a6bcae
zupanInden <97639601+zupanInden@users.noreply.github.com>
2023-03-17 10:07:04 +0100

    Changed from yarn to npm

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Checking if staff works!</Text>
       <StatusBar style="auto" />
     </View>
   );
