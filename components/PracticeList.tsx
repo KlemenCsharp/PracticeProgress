7827b46e74a5ab3558687186424ce851225810d0
zupanInden <97639601+zupanInden@users.noreply.github.com>
2023-03-18 07:56:55 +0100

    #1 - Created flat list of practices with dummy data

--- components/PracticeList.tsx
+++ components/PracticeList.tsx
@@ -1,9 +1,26 @@
-import {View, Text} from "react-native";
+import {View, Text, FlatList} from "react-native";
+import Practice from "./Practice";
+import practice from "./Practice";
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
 
 function PracticeList() {
     return (
         <View>
-            <Text>Practice list!</Text>
+            <FlatList data={DATA} renderItem={({practice}) => <Practice />} keyExtractor={practice.id} />
         </View>
     )
 }
