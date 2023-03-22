d70681979b906fe7e8e317f98b729cef4e492291
zupanInden <97639601+zupanInden@users.noreply.github.com>
2023-03-22 22:23:47 +0100

    #6 - Set up ESLint & Prettier

--- components/Practice.tsx
+++ components/Practice.tsx
@@ -1,46 +1,49 @@
-import {StyleSheet, View, Text} from "react-native";
-
-function Practice(props: any) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.main}>
-                <Text style={styles.title}>{props.practice.title}</Text>
-                <Text>{props.practice.startingTempo} - {props.practice.endingTempo}</Text>
-            </View>
-            <View style={styles.footer}>
-                <Text style={styles.description}>{props.practice.description}</Text>
-                <Text style={styles.status}>{props.practice.status}</Text>
-            </View>
-
-        </View>
-    )
-}
+import { StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        marginVertical: 6,
-        padding: 8,
-        borderRadius: 10
-    },
-    main: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginBottom: 2
-    },
-    title: {
-        fontSize: 18
-    },
-    footer: {
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    description: {
-        fontSize: 12
-    },
-    status: {
-        color: "green"
-    }
-})
+  container: {
+    backgroundColor: 'white',
+    marginVertical: 6,
+    padding: 8,
+    borderRadius: 10,
+  },
+  main: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 2,
+  },
+  title: {
+    fontSize: 18,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  description: {
+    fontSize: 12,
+  },
+  status: {
+    color: 'green',
+  },
+});
+
+// TODO: Popravi ko bos implementiral DTO-je oziroma modele
+// eslint-disable-next-line
+function Practice({ practice }: any) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>{practice.title}</Text>
+        <Text>
+          {practice.startingTempo} - {practice.endingTempo}
+        </Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.description}>{practice.description}</Text>
+        <Text style={styles.status}>{practice.status}</Text>
+      </View>
+    </View>
+  );
+}
 
 export default Practice;
--- theme.ts
+++ theme.ts
@@ -1,49 +1,49 @@
 const palette = {
-    purple: '#5A31F4',
-    green: '#0ECD9D',
-    red: '#CD0E61',
-    black: '#0B0B0B',
-    white: '#F0F2F3',
-}
+  purple: '#5A31F4',
+  green: '#0ECD9D',
+  red: '#CD0E61',
+  black: '#0B0B0B',
+  white: '#F0F2F3',
+};
 
 export const theme = {
-    colors: {
-        background: palette.white,
-        foreground: palette.black,
-        primary: palette.purple,
-        success: palette.green,
-        danger: palette.red,
-        failure: palette.red,
+  colors: {
+    background: palette.white,
+    foreground: palette.black,
+    primary: palette.purple,
+    success: palette.green,
+    danger: palette.red,
+    failure: palette.red,
+  },
+  spacing: {
+    s: 8,
+    m: 16,
+    l: 24,
+    xl: 40,
+  },
+  textVariants: {
+    header: {
+      fontFamily: 'Roboto',
+      fontSize: 36,
+      fontWeight: 'bold',
     },
-    spacing: {
-        s: 8,
-        m: 16,
-        l: 24,
-        xl: 40,
+    body: {
+      fontFamily: 'Roboto',
+      fontSize: 16,
     },
-    textVariants: {
-        header: {
-            fontFamily: 'Roboto',
-            fontSize: 36,
-            fontWeight: 'bold',
-        },
-        body: {
-            fontFamily: 'Roboto',
-            fontSize: 16,
-        },
-    },
-    breakpoints: {
-        smallPhone: 0,
-        phone: 321,
-        tablet: 768
-    }
-}
+  },
+  breakpoints: {
+    smallPhone: 0,
+    phone: 321,
+    tablet: 768,
+  },
+};
 
 export const darkTheme = {
-    ...theme,
-    colors: {
-        ...theme.colors,
-        background: palette.black,
-        foreground: palette.white,
-    }
-}
+  ...theme,
+  colors: {
+    ...theme.colors,
+    background: palette.black,
+    foreground: palette.white,
+  },
+};
